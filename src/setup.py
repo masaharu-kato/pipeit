b3a32178e9dc600ea0cb62d5ecbcda2d0868b4e5
Masaharu Kato <fudai.mk@gmail.com>
2020-04-07 10:33:50 +0900

    create module files

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pipeit',
+    version='1.0.0',
+    url='https://github.com/masaharu-kato/pipeit',
+    author='Masaharu Kato',
+    author_email='fudai.mk@gmail.com',
+    description='Pipeit module',
+    packages=find_packages(),
+    install_requires=[],
+)
